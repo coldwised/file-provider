66185e861b6b34b3ce97230434d5dd9e53deca70
summerwise <zelyoniyoleg@yandex.ru>
2023-05-10 23:51:40 +0500

    save

--- app/src/main/java/com/filemanager/presentation/main/type/SortType.kt
+++ app/src/main/java/com/filemanager/presentation/main/type/SortType.kt
@@ -2,5 +2,7 @@ package com.filemanager.presentation.main.type
 
 enum class SortType(val text: String) {
     ByName("по названию"),
-    BySize("по размеру")
+    BySize("по размеру"),
+    ByType("по расширению"),
+    ByDate("по дате")
 }
